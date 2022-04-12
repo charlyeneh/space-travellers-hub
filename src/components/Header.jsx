f6b49a6c652a113919df7b2ad6b5d03fd923577d
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-04-12 14:52:37 -0300

    Update initial Header for App

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import './Header.css';
 
 const Header = () => (
-  <header>
-    <nav>
-      <ul>        
-        <li>Rockets</li>
-        <li>Missions</li>
-        <li>Dragons</li>
-        <li>My Profile</li>
-      </ul>
-    </nav>
+  <header className="header-container">
+    <div className="header-inner">
+      <Link to="/" className="header-logo">
+        <img src={logo} alt="Logo" />
+        <div>
+          Space Travelers&apos; Hub
+        </div>
+      </Link>
+      <nav className="header-nav">
+        <ul>
+          <li>
+            <NavLink to="/rockets">
+              Rockets
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dragons">
+              Dragons
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/missions">
+              Missions
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/profile">
+              My Profile
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 );
-
 export default Header;
