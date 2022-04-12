883cefd6dad4eec2b2d313803f3ab46a4b850454
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-04-12 15:21:17 -0300

    Add message to test root router

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => (
   <Router>
     <Header />
     <Routes>
-      <Route path="/" element={<h1>hi</h1>} />
+      <Route path="/" element={<h1>Hello Project!</h1>} />
       <Route path="/rockets" element={<h1>rockets</h1>} />
       <Route path="/dragons" element={<h1>dragons</h1>} />
       <Route path="/missions" element={<h1>missions</h1>} />
