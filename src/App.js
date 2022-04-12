6aa9c5823426396aa5435904ff195f129b5a5ffe
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-04-12 14:53:10 -0300

    Update router links for App Header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import './App.css';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <div>
-        Hello Project
-      </div>
-    </>
-  );
-}
+const App = () => (
+  <Router>
+    <Header />
+    <Routes>
+      <Route path="/" element={<h1>hi</h1>} />
+      <Route path="/rockets" element={<h1>rockets</h1>} />
+      <Route path="/dragons" element={<h1>dragons</h1>} />
+      <Route path="/missions" element={<h1>missions</h1>} />
+      <Route path="/profile" element={<h1>profile</h1>} />
+    </Routes>
+  </Router>
+);
 
 export default App;
