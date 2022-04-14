e608fd73da7605f0757333385801b5b63d909850
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-04-14 13:13:22 -0300

    Update missions display list

--- src/components/missions/missions.jsx
+++ src/components/missions/missions.jsx
@@ -1,20 +1,68 @@
-import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { getMissionData } from '../../redux/missions/missions';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissionsData } from '../../redux/missions/missions';
+import './missions.css';
+
+let loadOance = false;
 
 const Missions = () => {
-  const missionsList = useSelector((state) => state.missionReducer);
   const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missionsReducer);
   useEffect(() => {
-    dispatch(getMissionData());
-  }, []);
+    if (!loadOance) {
+      dispatch(getMissionsData());
+      loadOance = true;
+    }
+  }, [dispatch]);
+
   return (
-    <div className="container missions">
-      <ul className="missions-list">
-        {missionsList.map((mission) => (
-          <li key={mission.id}>{`id: ${mission.id}, name: ${mission.name}`}</li>
-        ))}
-      </ul>
+    <div className="missions-container">
+      <table className="missions-table">
+        <thead>
+          <tr>
+            <th>
+              <h1>Mission</h1>
+            </th>
+            <th>
+              <h1>Description</h1>
+            </th>
+            <th>
+              <h1>Status</h1>
+            </th>
+            <th>
+              <h1>Join</h1>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map(({
+            id, name, description, reserved,
+          }) => (
+            <tr key={id}>
+              <td className="missions-name">
+                <h1>{name}</h1>
+              </td>
+              <td className="missions-description">
+                <p>{description}</p>
+              </td>
+              <td>
+                {reserved ? (
+                  <span className="badge badge--primary">ACTIVE MEMBER</span>
+                ) : (
+                  <span className="badge badge--secondary">NOT A MEMBER</span>
+                )}
+              </td>
+              <td>
+                {reserved ? (
+                  <span className="app-btn-danger">Leave Mission</span>
+                ) : (
+                  <span className="app-btn-danger">Join Mission</span>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
