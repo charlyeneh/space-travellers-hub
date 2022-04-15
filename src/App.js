097ccded92921408b4f252b526fc017634e8f8fd
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-04-15 20:53:35 -0300

    Update App Routes and Links

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Rockets from './components/Rockets';
+import Rockets from './components/rockets/rockets';
 import Missions from './components/missions/missions';
 import Profile from './components/profile/profile';
-import { getMissionsData } from './redux/missions/missions';
+import './App.css';
 
-const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getMissionsData());
-  }, []);
-  return (
-    <Router>
-      <Header />
+const App = () => (
+  <div className="App">
+    <Header />
+    <main className="main-app">
       <Routes>
-        <Route path="/" element={<h1>Hello Project!</h1>} />
         <Route path="/rockets" element={<Rockets />} />
         <Route path="/dragons" element={<h1>dragons</h1>} />
         <Route path="/missions" element={<Missions />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
-    </Router>
-  );
-};
+    </main>
+  </div>
+);
 
 export default App;
